3a5c81ef8796dfa0616c3462a2256c0b35267211
VERTEksaR <14.vert.41@gmail.com>
2024-05-17 13:41:56 +0300

    Класс с функциями для работы с ЯД перенесен в отдельный файл

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ from requests import ConnectionError
 
 import config_data
 
-URL = 'https://cloud-api.yandex.net/v1/disk/resources'
-TOKEN = config_data.TOKEN_CLOUD
-HEADERS = {'Authorization': f'OAuth {TOKEN}'}
+from template_cloud import FilesCloud
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(levelname)s | %(message)s')
+logging.basicConfig(level=logging.INFO, filename=config_data.PATH_TO_LOG_FILE,
+                    filemode='a', format='%(asctime)s | %(levelname)s | %(message)s')
 logger = logging.getLogger(__name__)
 
-FILE_TIME = {}
 DISK_FILES, COMP_FILES = [], []
 
 
@@ -27,7 +24,7 @@ def create_config():
     config.add_section('Settings')
     config.set('Settings', 'path_to_sync_dir', config_data.PATH_TO_SYNC_DIR)
     config.set('Settings', 'dir_name_cloud', config_data.DIR_NAME_CLOUD)
-    config.set('Settings', 'token_cloud', TOKEN)
+    config.set('Settings', 'token_cloud', config_data.TOKEN_CLOUD)
     config.set('Settings', 'period_of_sync', config_data.PERIOD_OF_SYNC)
     config.set('Settings', 'path_to_log_file', config_data.PATH_TO_LOG_FILE)
 
@@ -37,38 +34,23 @@ def create_config():
 
 
 def create_folder(path):
-    requests.put(f'{URL}?path={path}', headers=HEADERS)
+    requests.put(f'https://cloud-api.yandex.net/v1/disk/resources?path={path}',
+                 headers={'Authorization': f'OAuth {config_data.TOKEN_CLOUD}'})
 
 
 def check_folder_exists(path):
-    result = requests.get(f'{URL}?path={path}', headers=HEADERS)
+    result = requests.get(f'https://cloud-api.yandex.net/v1/disk/resources?path={path}',
+                          headers={'Authorization': f'OAuth {config_data.TOKEN_CLOUD}'})
     return result.status_code
-
-
-def post_file_to_disk(path, path_from_get, file=''):
-    try:
-        upload_file = {'file': open(path, 'rb')}
-        requests.put(path_from_get, files=upload_file)
-        return True
-    except FileNotFoundError:
-        logger.error(f'Файл {file} удалили сразу же, как только он был создан')
-
-
-def check_file_if_not_exists(cloud_path):
-    params = {'path': cloud_path}
-    result = requests.get(f'{URL}/upload', headers=HEADERS, params=params)
-
-    try:
-        href = result.json()['href']
-        return href
-    except KeyError:
-        return False
 
 
 def check_changes_in_file(path, time_file_comp):
     params = {'path': path}
-    result = requests.get(URL, headers=HEADERS, params=params)
-    mdate = datetime.strptime(result.json()['modified'].replace('T', ' ')[:-6], '%Y-%m-%d %H:%M:%S') + timedelta(hours=3)
+    result = requests.get('https://cloud-api.yandex.net/v1/disk/resources',
+                          headers={'Authorization': f'OAuth {config_data.TOKEN_CLOUD}'},
+                          params=params)
+    mdate = datetime.strptime(result.json()['modified'].replace('T', ' ')[:-6],
+                              '%Y-%m-%d %H:%M:%S') + timedelta(hours=3)
 
     if time_file_comp > mdate:
         return True
@@ -76,13 +58,9 @@ def check_changes_in_file(path, time_file_comp):
         return False
 
 
-def delete_file(path):
-    params = {'path': path, 'permanently': 'true'}
-    requests.delete(URL, headers=HEADERS, params=params)
-
-
 def get_list_all_files_disk():
-    result = requests.get(f'{URL}/files', headers=HEADERS).json()
+    result = requests.get(f'https://cloud-api.yandex.net/v1/disk/resources/files',
+                          headers={'Authorization': f'OAuth {config_data.TOKEN_CLOUD}'}).json()
 
     for file in result['items']:
         DISK_FILES.append(file['name'])
@@ -115,58 +93,56 @@ def first_settings():
         logger.info(f'Папка {config_data.DIR_NAME_CLOUD} создана')
 
 
-def rewrite_file(path, file):
-    delete_file(f'{config_data.DIR_NAME_CLOUD}/{file}')
-    href_reload = check_file_if_not_exists(f'{config_data.DIR_NAME_CLOUD}/{file}')
-    post_file_to_disk(path, href_reload)
-    logger.info(f'Файл {file} перезаписан')
-
-
-def check_delete_diff_files():
+def check_delete_diff_files(disk_class):
     diff = list(set(DISK_FILES) - set(COMP_FILES))
 
     if len(diff) != 0:
         for need_to_delete in diff:
-            delete_file(f'{config_data.DIR_NAME_CLOUD}/{need_to_delete}')
+            disk_class.delete(need_to_delete)
             DISK_FILES.remove(need_to_delete)
             logger.info(f'Удален файл {need_to_delete} при инициализации приложения')
 
 
-def main():
+def main(disk_class):
     get_list_all_files_disk()
     get_list_all_files_comp()
 
     for file in COMP_FILES:
         path = os.path.join('\\', config_data.PATH_TO_SYNC_DIR, file)
-        href = check_file_if_not_exists(f'{config_data.DIR_NAME_CLOUD}/{file}')
+        href = disk_class.check_file_exists(f'{config_data.DIR_NAME_CLOUD}/{file}')
 
         if href:
-            post_file_to_disk(path, href, file)
+            disk_class.load(file)
             logger.info(f'Файл {file} добавлен в ЯД')
 
         try:
             mtime = os.path.getmtime(path)
             mtime_readable = datetime.fromtimestamp(mtime)
-            result = check_changes_in_file(f'{config_data.DIR_NAME_CLOUD}/{file}', mtime_readable)
+            result = check_changes_in_file(f'{config_data.DIR_NAME_CLOUD}/{file}',
+                                           mtime_readable)
 
             if result:
-                rewrite_file(path, file)
+                disk_class.reload(file)
         except FileNotFoundError:
-            delete_file(f'{config_data.DIR_NAME_CLOUD}/{file}')
+            disk_class.delete(file)
             COMP_FILES.remove(file)
             logger.info(f'Файл {file} удален')
 
 
 if __name__ == '__main__':
+    disk = FilesCloud(url='https://cloud-api.yandex.net/v1/disk/resources',
+                      headers={'Authorization': f'OAuth {config_data.TOKEN_CLOUD}'},
+                      cloud_dir=config_data.DIR_NAME_CLOUD)
+
     first_settings()
     get_list_all_files_disk()
     get_list_all_files_comp()
-    check_delete_diff_files()
+    check_delete_diff_files(disk_class=disk)
+
     while True:
         try:
-            main()
+            main(disk_class=disk)
             time.sleep(int(config_data.PERIOD_OF_SYNC))
         except ConnectionError:
             logger.error('Проблема с подкючением')
             time.sleep(int(config_data.PERIOD_OF_SYNC))
-
